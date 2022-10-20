6a3cc52f85732c4192393aa612b84e9ee479c08a
a.sitnikov <a.sitnikov@unabank.com>
2022-10-20 23:39:45 +0500

    add kafka module

--- api-v1/src/main/kotlin/ApiV1Mapper.kt
+++ api-v1/src/main/kotlin/ApiV1Mapper.kt
@@ -2,7 +2,12 @@ package ru.artursitnikov.fitness.api.v1
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import ru.artursitnikov.fitness.api.v1.models.IRequest
 
 val apiV1Mapper = ObjectMapper().apply {
     configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T : IRequest> apiV1RequestDeserialize(json: String): T =
+    apiV1Mapper.readValue(json, IRequest::class.java) as T
---
+++ kafka/src/main/kotlin/common/Topic.kt
@@ -0,0 +1,5 @@
+package ru.artursitnikov.fitness.kafka.common
+
+const val TOPIC_IN = "program_in"
+
+const val TOPIC_OUT = "program_out"
