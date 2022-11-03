ad332c87bbdbf8d5b939be89be72edad5de19142
a.sitnikov <a.sitnikov@unabank.com>
2022-11-03 15:34:12 +0500

    add repo to spring

--- app-spring/src/main/kotlin/config/ServiceConfig.kt
+++ app-spring/src/main/kotlin/config/ServiceConfig.kt
@@ -1,11 +1,31 @@
 package ru.artursitnikov.fitness.appspring.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import ru.artursitnikov.fitness.biz.ProgramProcessor
+import ru.artursitnikov.fitness.common.models.Settings
+import ru.artursitnikov.fitness.repo.inmemory.ProgramRepoInmemory
+import ru.artursitnikov.fitness.repo.postgres.DbConfig
+import ru.artursitnikov.fitness.repo.postgres.ProgramRepoSql
 
 @Configuration
 class ServiceConfig {
+    @Autowired
+    lateinit var env: Environment
+
     @Bean
-    fun programProcessor(): ProgramProcessor = ProgramProcessor()
+    fun programProcessor(): ProgramProcessor {
+        return ProgramProcessor(
+            settings = Settings(
+                repoTest = ProgramRepoInmemory(),
+                repoProd = ProgramRepoSql(config = DbConfig(
+                    url = env.getProperty("database.url")!!,
+                    user = env.getProperty("database.user")!!,
+                    password = env.getProperty("database.password")!!,
+                    schema = env.getProperty("database.schema")!!,
+                ))
+            ))
+    }
 }
